e86e95d488c622a795c25a3603449bae5f4685f3
Kazım Bayram <kazim1001@gmail.com>
2017-03-25 23:21:43 +0300

    Initial Commit

---
+++ ArduinoCode/Serial.ino
@@ -0,0 +1,31 @@
+﻿#include <Arduino.h>
+
+/*
+Serial port communication on Computer Controller Arduino with C#
+Author: Kazım Bayram <kazim1001@gmail.com>
+*/
+
+byte status;
+
+
+
+void setup()
+{
+
+	Serial.begin(9600);
+	// Serial Veri kodları kodlar buraya gelecek;
+
+
+}
+
+
+void loop()
+{
+    if(Serial.available()){
+        status = Serial.read();
+        Serial.println(status);
+	delay(1000);
+        /// işlem kodları buraya gelecek kodu buraya gelcek;
+    }
+
+}
---
+++ ArduinoUI/ArduinoUI/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoUI
+{
+    public partial class Form1 : Form
+    {
+
+        private SerialPort serial;
+  
+        public Form1()
+        {
+            InitializeComponent();
+            Setup();
+        }
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+            Calistir();
+        }
+        private void button2_MouseUp(object sender, MouseEventArgs e)
+        {
+            Durdur();
+        }
+
+        private void Setup()
+        {
+            comboBox1.DataSource = SerialPort.GetPortNames();
+        }
+
+        private void Calistir()
+        {
+            serial.Write("1");
+            Debug.WriteLine("Gönderildi");
+        }
+
+        private void Durdur()
+        {
+            serial.Write("0");
+            Debug.WriteLine("Durduruldu");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serial = new SerialPort(comboBox1.Text, 9600);
+            serial.Open();
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serial != null && serial.IsOpen)
+            {
+                serial.Close();
+            }
+        }
+    }
+}
